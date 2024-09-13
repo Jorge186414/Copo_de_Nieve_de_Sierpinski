cf44329f94874695b7682182b24aca3b9c14e989
Jorge Miranda Zuniga <jorgaismiranda@gmail.com>
2024-09-13 11:07:26 -0600

    ADD: Metodo para pintar el copo de nieve

--- src/CopoDeNieve.java
+++ src/CopoDeNieve.java
@@ -1,4 +1,5 @@
 
+import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -23,8 +24,6 @@ public class CopoDeNieve extends JPanel {
     }
 
     protected void graficarCopo(Graphics g) {
-        super.graficarCopo(g);
-
         double xp1 = 400, yp1 = 100;
         double xp2 = 200, yp2 = 500;
         double xp3 = 600, yp3 = 500;
@@ -32,5 +31,28 @@ public class CopoDeNieve extends JPanel {
         pintarCopoDeNieve(g, recursividad, xp1, yp1, xp2, yp2);
         pintarCopoDeNieve(g, recursividad, xp2, yp2, xp3, yp3);
         pintarCopoDeNieve(g, recursividad, xp3, yp3, xp1, yp1);
+    }
+
+    private void pintarCopoDeNieve(Graphics g, int nivel, double xp1, double yp1, double xp2, double yp2) {
+        if (nivel == 0) {
+            g.setColor(Color.blue);
+            g.drawLine((int) xp1, (int) yp1, (int) xp2, (int) yp2);
+        } else {
+            double dx = (xp2 - xp1) / 3;
+            double dy = (yp2 - yp1 / 3);
+
+            double x1 = xp1 + dx;
+            double y1 = yp1 + dy;
+            double x2 = xp1 + 2 * dx;
+            double y2 = yp1 + 2 * dy;
+
+            double xx = (x1 + x2) / 2 - (dy * sin60);
+            double yy = (y1 + y2) / 2 + (dx * sin60);
+
+            pintarCopoDeNieve(g, nivel - 1, xp1, yp1, x1, y1);
+            pintarCopoDeNieve(g, nivel - 1, x1, y1, xx, yy);
+            pintarCopoDeNieve(g, nivel - 1, xx, yy, x2, y2);
+            pintarCopoDeNieve(g, nivel - 1, x2, y2, xp2, yp2);
+        }
     }
 }
